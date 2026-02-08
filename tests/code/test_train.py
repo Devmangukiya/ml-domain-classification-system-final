20e3ea345993a3d0ae36c767fde7915aa4ecd6e6
Devmangukiya <devmangukiya561@gmail.com>
2026-02-08 11:55:44 +0000

    Commit

---
+++ tests/code/test_train.py
@@ -0,0 +1,30 @@
+import os
+import pytest
+import utils
+from pathlib import Path
+from components import train
+
+
+@pytest.mark.training
+def test_train_model(dataset_loc):
+    experiment_name = utils.generate_experiment_name(prefix="test_train")
+    train_loop_config = Path("configs/train_loop_config.json")
+
+    assert train_loop_config.exists(), "train_loop_config.json not found"
+
+    result = train.train_model(
+        experiment_name=experiment_name,
+        dataset_loc=dataset_loc,
+        train_loop_config=str(train_loop_config),
+        num_workers=6,
+        cpu_per_worker=1,
+        gpu_per_worker=0,
+        num_epochs=2,
+        num_samples=512,
+        batch_size=256,
+        results_fp=None,
+    )
+
+    utils.delete_experiment(experiment_name=experiment_name)
+    train_loss_list = result.metrics_dataframe.to_dict()["train_loss"]
+    assert train_loss_list[0] > train_loss_list[1]   ## loss decreased
