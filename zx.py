da5661cb362a36a7da27d73c4a548cd131b01296
Devmangukiya <devmangukiya561@gmail.com>
2026-02-09 10:15:20 -0800

    Update code

--- zx.py
+++ zx.py
@@ -1,4 +1,7 @@
-from components.config import MLFLOW_TRACKING_URI, EFS_DIR
+from components.config import MLFLOW_TRACKING_URI, EFS_DIR, MODEL_REGISTRY
 
-print(EFS_DIR)
-print(MLFLOW_TRACKING_URI)
+
+
+print(EFS_DIR)                #   /home/ray/MLOPs_Project/Devmangukiya
+print(MLFLOW_TRACKING_URI)    #   file:///home/ray/MLOPs_Project/Devmangukiya/mlflow
+print(MODEL_REGISTRY)         #   /home/ray/MLOPs_Project/Devmangukiya/mlflow
